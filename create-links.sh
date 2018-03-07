7bd6668b2786afcad7251a3581a60b6f27e9c16c
Kai Eckert <gen-coding@kaiec.de>
2018-03-07 17:01:01 +0100

    Add todo.txt support

--- create-links.sh
+++ create-links.sh
@@ -4,3 +4,6 @@ ln -sTf ~/dotfiles/config/i3 ~/.config/i3
 ln -sTf ~/dotfiles/config/i3status ~/.config/i3status
 ln -sTf ~/dotfiles/vimrc ~/.vimrc
 ln -sTf ~/dotfiles/bashrc ~/.bashrc
+mkdir ~/.todo
+ln -sTf ~/dotfiles/todo.conf ~/.todo/config
+ln -sTf ~/dotfiles/bin/t ~/bin/t
