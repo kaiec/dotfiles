58cb70259faeee74ae882ffd698bd4306f2e8a2c
Kai Eckert <hallo@kaiec.de>
2018-10-14 19:31:37 +0200

    Better wakeup lock

--- bin/run-when-suspend.sh
+++ bin/run-when-suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISPLAY=:1
-/home/kai/.screenlayout/nb.sh
-sleep 5 
-/usr/bin/i3lock -e -c 000000
+/home/kai/.screenlayout/nb
+/bin/sleep 1
+/home/kai/dotfiles/bin/lock
 # Add dotfiles/suspend/lock@.service to /etc/systemctl/system
 # Enable with systemctl enable lock@kai.service
