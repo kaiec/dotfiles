f5922a19cb90cee851700715f0c2cede4db656e0
Kai Eckert <hallo@kaiec.de>
2018-10-15 15:47:49 +0200

    Fixes suspend lock

--- bin/run-when-suspend.sh
+++ bin/run-when-suspend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISPLAY=:1
-/home/kai/.screenlayout/nb
-/bin/sleep 1
 /home/kai/dotfiles/bin/lock
+/bin/sleep 2
+/home/kai/.screenlayout/nb
 # Add dotfiles/suspend/lock@.service to /etc/systemctl/system
 # Enable with systemctl enable lock@kai.service
